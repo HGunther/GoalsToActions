cc81315e01dd02fb36b3a64cbefb4aabe0e55949
hgunther <hnsgunther@gmail.com>
2019-04-16 21:48:37 -0600

    Updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GoalsToActions
 A to do list web app
+
+## Running the application
+Start the server
+```
+> cd Server
+> npm start
+```
+
+Start the client
+```
+> cd Client
+> npm start
+```
--- Server/app.js
+++ Server/app.js
@@ -1,21 +1,44 @@
+// import { TASKS } from './mock-tasks';
+
+// require('TASKS');
+
 const express = require('express')
 const app = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+const TASKS = [
+    {
+      id: 1,
+      title: "Finish this app",
+      description: "Keep doing what you're doing. You'll get there.",
+      date_created: new Date(2019, 3, 8, 21, 31),
+      date_due: new Date(2019, 4, 0, 21, 31),
+      date_start_by: new Date(2019, 3, 8, 21, 31),
+      complete: false
+    },
+    {
+      id: 2,
+      title: "Clean your room",
+      description: "Keep doing what you're doing. You'll get there.",
+      date_created: new Date(2019, 3, 8, 21, 31),
+      date_due: new Date(2019, 4, 0, 21, 31),
+      date_start_by: new Date(2019, 3, 8, 21, 31),
+      complete: false
+    }]
+
+
+
 app.get('/', function(req, res) {
-    console.log("Recieved " + req.method +  " at " + req.path)
-    res.status(200).send('You found me!')
+    console.log("Recieved " + req.method +  " at " + req.path);
+    res.status(200).send('You found me!');
 })
 
-// app.get('/app', (req, res) => res.send )
-
 app.get('/api/tasks', function (req, res) {
-    console.log("Recieved " + req.method +  " at " + req.path)
-    res.send({
-        some: 'json'
-    })
+    console.log("Recieved " + req.method +  " at " + req.path);
+    res.send(TASKS);
 })
 
 app.post('/api/tasks', function (req, res) {
@@ -33,6 +56,10 @@ app.delete('/api/tasks', function (req, res) {
     res.send('Got a DELETE request')
 })
 
+app.get('*', function(req, res) {
+    console.log("Recieved " + req.method +  " at " + req.path)
+    res.status(404).send('You done messed up!');
+})
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
